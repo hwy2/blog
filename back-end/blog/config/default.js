2cc8e415876174fa6f665ce05665369481fe6a5b
hwy2 <40650839+hwy2@users.noreply.github.com>
2021-08-31 09:18:47 +0800

    Update default.js

--- back-end/blog/config/default.js
+++ back-end/blog/config/default.js
@@ -19,9 +19,9 @@ var configs = {
         secretOrPrivateKey: 'iboomer', // 秘钥
     },
     email: { //默认邮箱
-        service: "1833121448@qq.com",
-        spassword: "dcsosrkhkvsifcjh",
-        admin: "1833121448@qq.com",
+        service: "xxxxxx@qq.com",
+        spassword: "xxxxxx",
+        admin: "xxxxxx@qq.com",
     },
     page: { //默认分页
         currPage: 1,
@@ -51,7 +51,7 @@ var configs = {
         face: 'http://localhost:3000/attchments/face/',
     },
     weather: {
-        key: "d2a66877783ac05400f583f04ececd0d",//高德地图的key
+        key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",//高德地图的key
         citycode: '440113' //番禺区
     },
     isFirstTimeInstall: false, //第一次运行之后请确保是false，否则会重建数据库
